fdd74dadbf4c97f081fe2d10ea405fbb5a3afe70
Fanny Ardant <s0552607@htw-berlin.de>
2025-01-13 21:43:31 +0100

    merge old commit with new commit

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # Ziel
 
-Das Hauptziel dieses Projekts ist es, historische Aktienkurse von Tesla, Ford und VW AG zu analysieren und deren zukünftige Kursentwicklungen vorherzusagen. Es kombiniert datengetriebene Methoden mit Recurrent Neural Networks (LSTM und RNN), um komplexe Zeitreihenmuster zu erkennen. Es muss herausfinden, welche Modell(LSTM und RNN) bessere Vorhersagen für Aktienkurse liefert. Und es hilft das Anlegern bei Entscheidungen, indem es zukünftige Preisentwicklung mit unterschiedlichen Features vorhersagt. 
+<mark> </mark>
 
 # Installation
 
@@ -11,6 +11,10 @@ Das Hauptziel dieses Projekts ist es, historische Aktienkurse von Tesla, Ford un
 * yfinance
 * sklearn
 * Qt Plot
+
+<code> pip install tensor flow </code>
+<code> pip install yfinance  </code>
+<code> pip install sklearn </code>
 
 # Test Scenario
 
@@ -24,18 +28,4 @@ Das Hauptziel dieses Projekts ist es, historische Aktienkurse von Tesla, Ford un
 
 # Problem
 
-Während des Projekts traten verschiedene Herausforderungen auf:
-
-Themenfindung: Es war schwierig, zu Beginn ein passendes Thema zu finden, das sowohl relevant als auch umsetzbar war.
-
-Datenverfügbarkeit: Die Suche nach geeigneten Datenquellen war zeitaufwendig und nicht alle relevanten Daten (z. B. Rohstoffpreise) waren für beide Unternehmen gleichermaßen verfügbar.
-
-Modellauswahl: Die Entscheidung, welche Modelle (z. B. LSTM, ARIMA) verwendet werden sollten, erforderte umfangreiche Recherche und Tests.
-
-Datenqualität: Einige historische Daten waren unvollständig oder enthielten Ausreißer, was eine umfangreiche Vorverarbeitung erforderte.
-
-Zeitaufwand: Die Implementierung und Optimierung von Modellen wie LSTM war zeitintensiv.
-
 # Zusammenfassung
-
-Das Projekt hat gezeigt, dass der Vergleich der Gewinne von Tesla und VW durch eine datengetriebene Analyse möglich ist. Die spezifischen finanziellen Strukturen und Marktbedingungen der beiden Unternehmen wurden berücksichtigt, um eine differenzierte Bewertung zu ermöglichen. Die Nutzung von Zeitreihenmodellen wie LSTM bietet eine vielversprechende Möglichkeit, Vorhersagen zu treffen. Trotz anfänglicher Herausforderungen, wie der Themenfindung, der Suche nach passenden Daten und der Auswahl geeigneter Modelle, konnte ein strukturierter Ansatz entwickelt werden. Dies legt die Grundlage für präzisere Analysen und Vorhersagen in zukünftigen Projekten.
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 # Make sure that you have all these libaries available to run the code successfully
 import os
+
 import matplotlib
 import numpy as np
 import pandas as pd
 import yfinance as yf
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from requests.packages import target
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
+
 # Preprocess data
 def preprocess_data(data, feature='Daily Close-Open', lookback=60):
     data = data[[feature]].values
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(data)
+    min_max_scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = min_max_scaler.fit_transform(data)
 
     x, y = [], []
     for i in range(lookback, len(scaled_data)):
@@ -26,14 +26,14 @@ def preprocess_data(data, feature='Daily Close-Open', lookback=60):
 
     x, y = np.array(x), np.array(y)
     x = np.reshape(x, (x.shape[0], x.shape[1], 1))  # LSTM input shape
-    return x, y, scaler
+    return x, y, min_max_scaler
 
 
 # Create LSTM model
 def create_lstm_model(input_shape):
     keras_model = tf.keras.Sequential([
         LSTM(50, return_sequences=True, input_shape=input_shape),
-        Dropout(0.2),  # 20% reduziert
+        Dropout(0.2),
         LSTM(50, return_sequences=False),
         Dropout(0.2),
         Dense(25),
@@ -42,21 +42,12 @@ def create_lstm_model(input_shape):
     keras_model.compile(optimizer='adam', loss='mean_squared_error')
     return keras_model
 
-def create_rnn_model(input_shape):
-    keras_model = tf.keras.Sequential([
-        tf.keras.layers.SimpleRNN(50, return_sequences=True, input_shape=input_shape),
-        Dropout(0.2),
-        tf.keras.layers.SimpleRNN(50, return_sequences=False),
-        Dropout(0.2),
-        Dense(25),
-        Dense(1)
-    ])
-    keras_model.compile(optimizer='adam', loss='mean_squared_error')
-    return keras_model
+
 # Train and evaluate the model
-def train_model(model, x_train, y_train, epochs=10, batch_size=32):
-    history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1)
-    return model, history
+def train_model(model, x_train, y_train, epochs=50, batch_size=32):
+    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
+    return model
+
 
 # Predict future prices
 def predict_future_price(model, data, lookback, scaler):
@@ -68,77 +59,79 @@ def predict_future_price(model, data, lookback, scaler):
     x_test = np.array(x_test)
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
     train_predictions = model.predict(x_test)
-    return scaler.inverse_transform(train_predictions)
+    train_predictions = scaler.inverse_transform(train_predictions)
+    return train_predictions
 
-# Datenaufbereitung für Analyse
+
 def get_filter_relevant_history_open_low_close_volume(histories):
-    histories['Daily Close-Open'] = histories['Close'] - histories['Open']
-    return histories[['Open', 'High', 'Low','Close', 'Volume', 'Daily Close-Open']]
+    dict_histories = histories[['Open', 'High', 'Low', 'Close', 'Volume']].to_dict('records')
+    list_of_dates = []
+    for date in histories.axes:
+        for value in date.values:
+            if type(value) is not str:
+                time_str = np.datetime_as_string(value, unit='D')
+                list_of_dates.append(time_str)
+
+    columns = ['Open', 'High', 'Low', 'Close', 'Volume', 'Date', 'Daily Close-Open']
+    counter = 0
+
+    for dict_history in dict_histories:
+        dict_history['Date'] = list_of_dates[counter]
+        diff = dict_history['Close'] - dict_history['Open']
+        dict_history['Daily Close-Open'] = diff
+        counter += 1
+
+    filter_dp = pd.DataFrame(dict_histories, columns=columns)
+    return filter_dp
 
 # period is for one year
 def get_company_filter_history(symbols_company, period="1y"):
-    # Autohersteller market als ticker
+    # Volkswagen AG market als ticker
     company_ticker = yf.Ticker(symbols_company)
+
     company_short_name = company_ticker.info.get("shortName")
-    print(f"Fetching data for {company_ticker.info.get('company_short_name')}...")
-    history = company_ticker.history(period=period)
-    filter_histories = get_filter_relevant_history_open_low_close_volume(history)
+    print(company_short_name)
+    histories_company = company_ticker.history(period=period)
+    filter_histories = get_filter_relevant_history_open_low_close_volume(histories_company)
+    print(filter_histories)
     # get Volkswagen financials per quarter
+
     return filter_histories, company_short_name
 
-# Plot Funktion
-def plot_results(company_name, actual_price, lstm_predictions, rnn_predictions, lookback=60):
-    plt.figure(figsize=(12, 6))
-    plt.plot(actual_price.index[lookback:], actual_price[target_property][lookback:],
-                 label="Actual Prices")
-    plt.plot(actual_price.index[lookback:], lstm_predictions, label="LSTM Predicted Prices")
-    plt.plot(actual_price.index[lookback:], rnn_predictions, label="RNN Predicted Prices")
-    plt.title(f'{company_name} Stock Price Prediction')
-    plt.xlabel("Date")
-    plt.ylabel("Price")
-    plt.legend()
-    plt.show()
 
+list_target_companies = ["VWAGY", "TSLA","NSANY","HMC","TM"]
 
-# Plot_loss function
-def plot_loss(company_name, history, model_type):
-    plt.figure(figsize=(8, 5))
-    plt.plot(history.history['loss'], label=f'{model_type} Training Loss')
-    plt.title(f'{company_name} Training Loss ({model_type})')
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.legend()
-    plt.show()
-
-# List of Company
-list_target_companies = ["VWAGY", "TSLA", "FORD"]
-lookback = 60 # Days of historical data for prediction
 target_property = 'Close'
+actual_property_label = 'Actual Prices'
+predicted_property_label = 'Predicted Prices'
 
-# Loop for List of Company
-for symbols_company in list_target_companies:
-    get_history, company_name = get_company_filter_history(symbols_company)
+matplotlib.use('TkAgg')
+fig = matplotlib.pyplot.figure()
+fig.canvas.draw()
 
-    if get_history.empty:
-        print(f"Data for {company_name} is empty. Skipping.")
-        continue
+for company in list_target_companies:
+    get_history, company_name = get_company_filter_history(company)
+    lookback = 60  # Days of historical data for prediction
 
     # Preprocess data
     x_train, y_train, scaler = preprocess_data(get_history, feature=target_property, lookback=lookback)
 
     # Create model
-    lstm_create_model = create_lstm_model((x_train.shape[1], 1))
-    rnn_create_model = create_rnn_model((x_train.shape[1], 1))
+    create_model = create_lstm_model((x_train.shape[1], 1))
 
     # Train model
-    lstm_model, lstm_history = train_model(lstm_create_model, x_train, y_train, epochs=10)
-    rnn_model, rnn_history = train_model(rnn_create_model, x_train, y_train, epochs=10)
+    model = train_model(create_model, x_train, y_train, epochs=10, batch_size=32)
 
     # Predict future prices
-    lstm_predictions = predict_future_price(lstm_model, get_history[[target_property]].values, lookback, scaler)
-    rnn_predictions = predict_future_price(rnn_model, get_history[[target_property]].values, lookback, scaler)
+    predictions = predict_future_price(model, get_history[[target_property]].values, lookback, scaler)
 
-    # Einzelne Grafiken zeigen
-    plot_results(company_name, get_history, lstm_predictions, rnn_predictions, lookback)
-    plot_loss(company_name, lstm_history, "LSTM")
-    plot_loss(company_name, rnn_history, "RNN")
+    # Plot results
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(get_history.index[lookback:], get_history[target_property].iloc[lookback:], label=actual_property_label)
+    plt.plot(get_history.index[lookback:], predictions, label=predicted_property_label)
+    plt.title(f"{company_name} Stock Price Prediction")
+    plt.xlabel("Date")
+    plt.ylabel("Price")
+    plt.legend()
+    plt.show()
