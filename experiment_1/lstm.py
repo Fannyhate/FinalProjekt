09ae856f92b743f584db8fb756d322756e4f5aaf
Fanny Ardant <s0552607@htw-berlin.de>
2025-01-19 13:58:08 +0100

    optimed double code to a class and added interface for modell function

--- experiment_1/lstm.py
+++ experiment_1/lstm.py
@@ -3,25 +3,23 @@ import os
 from abc import ABC
 
 import tensorflow as tf
-from keras.src.layers import SimpleRNN
-from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import LSTM, Dense, Dropout
 
-from trainer.training import TrainModel, calculate_mde, predict_future_price, train_model, calculate_mse, ITrainingModel
+from trainer.training import TrainModel, train_model, predict_future_price, calculate_mde, calculate_mse, ITrainingModel
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 
-# class inheritance from ItrainingModel
-class RNNModel(ITrainingModel, ABC):
+class LSTMModel(ITrainingModel, ABC):
     def __init__(self):
         super().__init__()
 
-    # Create RNN Model
+    # Create LSTM Model
     def create_model(self, input_shape):
         keras_model = tf.keras.Sequential([
-            SimpleRNN(50, return_sequences=True, input_shape=input_shape),
+            LSTM(50, return_sequences=True, input_shape=input_shape),
             Dropout(0.2),
-            SimpleRNN(50, return_sequences=False),
+            LSTM(50, return_sequences=False),
             Dropout(0.2),
             Dense(25),
             Dense(1)
@@ -47,25 +45,25 @@ for symbols_company in list_target_companies:
     # Preprocess data
     x_train, y_train, scaler = training.preprocess_data(get_history, lookback=lookback)
 
-    # Create model RNN
-    rnn = RNNModel()
-
-    rnn_create_model = rnn.create_model((x_train.shape[1], 1))
+    # Create model
+    lstm = LSTMModel()
+    lstm_create_model = lstm.create_model((x_train.shape[1], 1))
 
     # Train model
-    rnn_model, rnn_history = train_model(rnn_create_model, x_train, y_train, epochs=30)
+    lstm_model, lstm_history = train_model(lstm_create_model, x_train, y_train, epochs=30)
 
     # Predict future prices
-    rnn_predictions = predict_future_price(rnn_model, get_history[[target_property]].values, lookback, scaler)
+    lstm_predictions = predict_future_price(lstm_model, get_history[[target_property]].values, lookback,
+                                            scaler)
 
-    mde = calculate_mde(get_history[target_property][lookback:].values, rnn_predictions)
+    mde = calculate_mde(get_history[target_property][lookback:].values, lstm_predictions)
     print(f"{company_name} MDE: {mde:.4f}")
 
     # Berechnung des MSE
-    mse = calculate_mse(get_history[target_property][lookback:].values, rnn_predictions)
+    mse = calculate_mse(get_history[target_property][lookback:].values, lstm_predictions)
     print(f"{company_name} MSE: {mse:.4f}")
 
     # Einzelne Grafiken zeigen
-    training.plot_results(get_history, rnn_predictions, "RNN Predicted Prices", lookback)
-    training.plot_loss(rnn_history, "RNN")
+    training.plot_results(get_history, lstm_predictions, "LSTM Predicted Prices", lookback)
+    training.plot_loss(lstm_history, "LSTM")
 #
